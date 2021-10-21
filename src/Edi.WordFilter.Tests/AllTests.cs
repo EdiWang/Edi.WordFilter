427110a714340f7a795c6600af27d981b53ba031
Edi Wang <Edi.Wang@outlook.com>
2021-10-21 13:27:11 +0800

    target .NET 6

--- src/Edi.WordFilter.Tests/AllTests.cs
+++ src/Edi.WordFilter.Tests/AllTests.cs
@@ -1,50 +1,49 @@
 using NUnit.Framework;
 
-namespace Edi.WordFilter.Tests
+namespace Edi.WordFilter.Tests;
+
+[TestFixture]
+public class Tests
 {
-    [TestFixture]
-    public class Tests
+    public IMaskWordFilter MaskWordFilter { get; set; }
+
+    [Test]
+    public void HarmonizeWords()
     {
-        public IMaskWordFilter MaskWordFilter { get; set; }
+        MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
 
-        [Test]
-        public void HarmonizeWords()
-        {
-            MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
+        var disharmonyStr = "Go fuck yourself and eat some shit!";
+        var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
+        Assert.AreEqual("Go **** yourself and eat some ****!", harmonyStr);
+    }
 
-            var disharmonyStr = "Go fuck yourself and eat some shit!";
-            var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
-            Assert.AreEqual("Go **** yourself and eat some ****!", harmonyStr);
-        }
+    [Test]
+    public void HarmonizeWords_MessedUpSource()
+    {
+        MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit|"));
 
-        [Test]
-        public void HarmonizeWords_MessedUpSource()
-        {
-            MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit|"));
+        var disharmonyStr = "Go fuck yourself and eat some shit!";
+        var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
+        Assert.AreEqual("Go **** yourself and eat some ****!", harmonyStr);
+    }
 
-            var disharmonyStr = "Go fuck yourself and eat some shit!";
-            var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
-            Assert.AreEqual("Go **** yourself and eat some ****!", harmonyStr);
-        }
+    [Test]
+    public void ContainsAnyWord_Yes()
+    {
+        MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
 
-        [Test]
-        public void ContainsAnyWord_Yes()
-        {
-            MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
+        var disharmonyStr = "Go fuck yourself and eat some shit!";
+        var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
+        Assert.IsTrue(b);
+    }
 
-            var disharmonyStr = "Go fuck yourself and eat some shit!";
-            var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
-            Assert.IsTrue(b);
-        }
+    [Test]
+    public void ContainsAnyWord_No()
+    {
+        MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
 
-        [Test]
-        public void ContainsAnyWord_No()
-        {
-            MaskWordFilter = new MaskWordFilter(new StringWordSource("fuck|shit"));
-
-            var disharmonyStr = "Go frack yourself and eat some shirt!";
-            var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
-            Assert.IsFalse(b);
-        }
+        var disharmonyStr = "Go frack yourself and eat some shirt!";
+        var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
+        Assert.IsFalse(b);
     }
 }
