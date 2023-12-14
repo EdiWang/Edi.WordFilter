ecc117f3bd0203a44751c4e42b4d7b9ef47235c8
Edi Wang <Edi.Wang@outlook.com>
2023-12-14 12:21:00 +0800

    extract interface file

--- src/Edi.WordFilter/HashTableWordFilter.cs
+++ src/Edi.WordFilter/HashTableWordFilter.cs
@@ -4,12 +4,6 @@ using System.Text;
 
 namespace Edi.WordFilter
 {
-    public interface IMaskWordFilter
-    {
-        bool ContainsAnyWord(string content);
-        string FilterContent(string content);
-    }
-
     public class HashTableWordFilter : IMaskWordFilter
     {
         private readonly IDictionary<char, IDictionary> _filterWords = new Dictionary<char, IDictionary>();
---
+++ src/Edi.WordFilter/IMaskWordFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace Edi.WordFilter;
+
+public interface IMaskWordFilter
+{
+    bool ContainsAnyWord(string content);
+    string FilterContent(string content);
+}
