8f9201f02965fc13b73e75a287357c2a8332fe2c
Edi Wang <Edi.Wang@outlook.com>
2023-12-14 12:21:57 +0800

    Create TrieTreeWordFilter.cs

---
+++ src/Edi.WordFilter/TrieTreeWordFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edi.WordFilter;
+
+public class TrieTreeWordFilter : IMaskWordFilter
+{
+    public bool ContainsAnyWord(string content)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string FilterContent(string content)
+    {
+        throw new NotImplementedException();
+    }
+}
