cbf1e2602c43f8bf697e13a944ee1048c2923a30
Edi Wang <Edi.Wang@outlook.com>
2023-12-14 12:37:35 +0800

    Create TrieTreeWordFilterTests.cs

---
+++ src/Edi.WordFilter.Tests/TrieTreeWordFilterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edi.WordFilter.Tests;
+
+[TestFixture]
+public class TrieTreeWordFilterTests
+{
+    public IMaskWordFilter MaskWordFilter { get; set; }
+
+
+}
