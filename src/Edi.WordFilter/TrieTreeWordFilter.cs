182150dec8d1bd75c501804932580a08e0bd6846
Edi Wang <Edi.Wang@outlook.com>
2023-12-14 12:30:08 +0800

    AddWord

--- src/Edi.WordFilter/TrieTreeWordFilter.cs
+++ src/Edi.WordFilter/TrieTreeWordFilter.cs
@@ -14,6 +14,29 @@ public class TrieNode
 
 public class TrieTreeWordFilter : IMaskWordFilter
 {
+    private TrieNode root = new TrieNode();
+
+    public TrieTreeWordFilter(IWordSource wordSource)
+    {
+        var banWords = wordSource.GetWordsArray();
+        foreach (var s in banWords) AddWord(s);
+    }
+
+    public void AddWord(string word)
+    {
+        TrieNode current = root;
+        foreach (char ch in word)
+        {
+            if (!current.Children.TryGetValue(ch, out TrieNode node))
+            {
+                node = new TrieNode();
+                current.Children.Add(ch, node);
+            }
+            current = node;
+        }
+        current.IsEndOfWord = true;
+    }
+
     public bool ContainsAnyWord(string content)
     {
         throw new NotImplementedException();
