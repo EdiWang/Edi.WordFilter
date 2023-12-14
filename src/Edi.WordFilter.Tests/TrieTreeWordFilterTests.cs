4306cd3d093383011ae22fa412336c9721ea925f
Edi Wang <Edi.Wang@outlook.com>
2023-12-14 12:40:52 +0800

    HarmonizeWords Test

--- src/Edi.WordFilter.Tests/TrieTreeWordFilterTests.cs
+++ src/Edi.WordFilter.Tests/TrieTreeWordFilterTests.cs
@@ -12,5 +12,13 @@ public class TrieTreeWordFilterTests
 {
     public IMaskWordFilter MaskWordFilter { get; set; }
 
+    [Test]
+    public void HarmonizeWords()
+    {
+        MaskWordFilter = new TrieTreeWordFilter(new StringWordSource("fuck|shit"));
 
+        var disharmonyStr = "Go fuck yourself and eat some shit!";
+        var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
+        Assert.That("Go **** yourself and eat some ****!", Is.EqualTo(harmonyStr));
+    }
 }
