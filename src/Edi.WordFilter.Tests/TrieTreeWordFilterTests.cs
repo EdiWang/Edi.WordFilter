894838c03f0609e6276879517e1a1f42899786ad
Edi Wang <Edi.Wang@outlook.com>
2023-12-15 12:16:39 +0800

    add UT

--- src/Edi.WordFilter.Tests/TrieTreeWordFilterTests.cs
+++ src/Edi.WordFilter.Tests/TrieTreeWordFilterTests.cs
@@ -26,4 +26,24 @@ public class TrieTreeWordFilterTests
         var harmonyStr = MaskWordFilter.FilterContent(disharmonyStr);
         Assert.That("Go **** yourself and eat some ****!", Is.EqualTo(harmonyStr));
     }
+
+    [Test]
+    public void ContainsAnyWord_Yes()
+    {
+        MaskWordFilter = new TrieTreeWordFilter(new StringWordSource("fuck|shit"));
+
+        var disharmonyStr = "Go fuck yourself and eat some shit!";
+        var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
+        Assert.That(b, Is.True);
+    }
+
+    [Test]
+    public void ContainsAnyWord_No()
+    {
+        MaskWordFilter = new TrieTreeWordFilter(new StringWordSource("fuck|shit"));
+
+        var disharmonyStr = "Go frack yourself and eat some shirt!";
+        var b = MaskWordFilter.ContainsAnyWord(disharmonyStr);
+        Assert.That(b, Is.False);
+    }
 }
