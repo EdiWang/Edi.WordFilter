76ccf82b5a65b05c42acb1b672f39558697ad5d6
Edi Wang <Edi.Wang@outlook.com>
2023-12-17 13:28:02 +0800

    Obsolete HashTableWordFilter

--- src/Edi.WordFilter/HashTableWordFilter.cs
+++ src/Edi.WordFilter/HashTableWordFilter.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Edi.WordFilter
 {
+    [Obsolete]
     public class HashTableWordFilter : IMaskWordFilter
     {
         private readonly IDictionary<char, IDictionary> _filterWords = new Dictionary<char, IDictionary>();
